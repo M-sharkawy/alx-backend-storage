ac22f11981fbbbb967ea1e7528b23c69e10a5fc0
M-sharkawy <mostafasharks021@gmail.com>
2025-05-02 17:36:29 +0300

    task 7 edits

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,11 +1,12 @@
 --  creates a stored procedure 'ComputeAverageScoreForUser' that computes and store the average score for a student
-DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser
-DELIMITER $$
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER $$ 
 CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
     DECLARE AVG_SCORE FLOAT DEFAULT 0;
 
-    SELECT AVG(score) INTO AVG_SCORE
+    SELECT AVG(score)
+    INTO AVG_SCORE
     FROM corrections
     WHERE corrections.user_id = user_id;
 
