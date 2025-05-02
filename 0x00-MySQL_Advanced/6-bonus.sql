296d3b1057d4b3f4c19cf7866fd52ac2e5aeeec5
M-sharkawy <mostafasharks021@gmail.com>
2025-05-02 15:21:59 +0300

    task 6

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,20 +1,22 @@
 --  creates a stored procedure AddBonus that adds a new correction for a student.
 DROP PROCEDURE IF EXISTS AddBonus;
 DELIMITER $$
-CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), OUT score INT)
+CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
-    DECLARE project_exist INT DEAFULT 0;
 
-    SELECT COUNT(id) INTO project_exist
-    FROM users
+    DECLARE project_exists INT DEFAULT 0;
+
+    SELECT COUNT(id)
+    INTO project_exists
+    FROM projects
     WHERE name = project_name;
 
-    IF project_exist = 0 THEN
-        INSERT INTO project (name)
+    IF project_exists = 0 THEN
+        INSERT INTO projects (name)
         VALUES (project_name);
     END IF;
 
-    INSERT INTO correction (user_id, project_id, score)
+    INSERT INTO corrections (user_id, project_id, score)
     VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
 END $$
 DELIMITER ;
