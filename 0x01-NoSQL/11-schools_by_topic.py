eb15dae15533f91d71402ec65560435e0bf71c26
M-sharkawy <mostafasharks021@gmail.com>
2025-05-07 14:44:06 +0300

    task 11

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""model returns the list of school having a specific topic"""
+from typing import List
+
+
+def schools_by_topic(mongo_collection, topic: str) -> List[str]:
+    """
+    returns the list of school having a specific topic
+    """
+    return list(mongo_collection.find({"topic": topic}))
