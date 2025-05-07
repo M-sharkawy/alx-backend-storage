8b6b9da2dd90e1ce7cf7e75f61ba4cd8ad51a241
M-sharkawy <mostafasharks021@gmail.com>
2025-05-07 14:23:23 +0300

    task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""model to inserts a new document
+in a collection based on kwargs"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """inserts a new document
+    in a collection based on kwargs
+    retrun id
+    """
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
